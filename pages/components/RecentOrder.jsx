585f051ce070aa260f57521527b011998bf1ef31
Emmanuel Essien <ehmaessien@gmail.com>
2023-04-11 23:42:38 +0100

    Update RecentOrder component

--- pages/components/RecentOrder.jsx
+++ pages/components/RecentOrder.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {data} from '../data/data'
+import {FaShoppingBag} from 'react-icons/fa'
 
 const RecentOrder = () => {
   return (
-    <div>
-      fvfsdvdvd
+    <div className='w-full col-span-1 relative lg:h-[70vh] h-[50vh] m-auto p-4 border rounded-lg bg-white overflow-scroll'>
+      <h1>Recent Orders</h1>
+      <ul>
+        {data.map(order => (
+          <li key={order.id} className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 flex items-center cursor-pointer'>
+            
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
